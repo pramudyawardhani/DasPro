677319fe5c81aaa4261f9108ff0a8f4448bc0da8
pramudyawardhani <radhanie04@gmail.com>
2026-08-28 19:10:46 +0700

    Add files via upload

---
+++ DasPro/Project/Latihan07.java
@@ -0,0 +1,7 @@
+public class Latihan07 {
+    public static void main(String[] args) {
+        System.out.println("Hi Saya Rara");
+        System.out.println("Belajar Java itu mudah loh!");
+        //System.out.println("Semakin sering mencoba, semakin jago.");
+    }
+}

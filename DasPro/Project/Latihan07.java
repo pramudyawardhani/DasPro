d33a7f982bc44b50c0f3607ec05dd7648faebbce
pramudyawardhani <radhanie04@gmail.com>
2026-09-02 09:34:29 +0700

    Delete DasPro/Project/Latihan07.java

--- DasPro/Project/Latihan07.java
+++
@@ -1,7 +0,0 @@
-public class Latihan07 {
-    public static void main(String[] args) {
-        System.out.println("Hi Saya Rara");
-        System.out.println("Belajar Java itu mudah loh!");
-        //System.out.println("Semakin sering mencoba, semakin jago.");
-    }
-}
